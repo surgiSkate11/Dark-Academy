492a3b9f65ee2f2c8a89651dc5f613ef1a061ca7
surgiSkate11 <danipales1122@gmail.com>
2024-09-13 22:25:41 -0500

    Mejora del archivo WaterFloat.cs

--- Assets/Scripts/WaterFloat.cs
+++ Assets/Scripts/WaterFloat.cs
@@ -1,25 +1,32 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaterFloat : MonoBehaviour {
+public class FlotarEnAgua : MonoBehaviour
+{
 
+    // Esta variable define la altura a la cual el objeto debería flotar en el agua
+    public float AlturaDelAgua = 15.5f;
 
-	public float WaterHeight = 15.5f;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // El método "Start" se ejecuta una vez al inicio cuando el objeto es creado o activado
+    // En este caso no estamos inicializando nada en este método
+    void Start()
+    {
+        // Código de inicialización (si fuera necesario)
+    }
 
-		if (transform.position.y < WaterHeight) {
-			//Vector3 newposition = Vector3 (transform.position.x, WaterHeight, transform.position.y);
-			//transform.position.y = newposition;
+    // El método "Update" se ejecuta una vez por cada frame (fotograma) del juego
+    void Update()
+    {
 
-			transform.position = new Vector3 (transform.position.x, WaterHeight, transform.position.z);
-		}
-		
-	}
+        // Verificamos si la posición del objeto (en el eje Y) está por debajo del nivel del agua
+        if (transform.position.y < AlturaDelAgua)
+        {
+
+            // Si el objeto está debajo del nivel del agua, ajustamos su posición para que flote.
+            // Creamos una nueva posición en el mismo punto (x, z) pero con la altura del agua (AlturaDelAgua) en el eje Y.
+            transform.position = new Vector3(transform.position.x, AlturaDelAgua, transform.position.z);
+        }
+
+    }
 }
