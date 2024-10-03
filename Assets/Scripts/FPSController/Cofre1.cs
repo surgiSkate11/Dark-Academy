d60fbad35f930d91b71bdbe1a90ed4eae54c0ef9
surgiSkate11 <danipales1122@gmail.com>
2024-10-03 09:32:40 -0500

    Add files via upload

---
+++ Assets/Scripts/FPSController/Cofre1.cs
@@ -0,0 +1,19 @@
+//using UnityEngine;
+
+//public class Chest1 : MonoBehaviour
+//{
+//    public GameObject ImageA; // Asigna la ImageA de la letra A desde el Inspector
+
+//    private void Start()
+//    {
+//        ImageA.SetActive(false); // Asegúrate de que la ImageA esté oculta al inicio
+//    }
+
+//    public void SoltarLlave()
+//    {
+//        // Aquí puedes desactivar el cofre y mostrar la ImageA
+//        gameObject.SetActive(false); // Desactiva el cofre
+//        ImageA.SetActive(true); // Activa la ImageA de la letra A
+//        Debug.Log("Cofre 1 abierto y la ImageA A mostrada.");
+//    }
+//}
